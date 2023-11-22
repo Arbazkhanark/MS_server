084c1f594dda407b78e1560ea55d707532de044d
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-11-22 17:07:49 +0530

    try 7

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const adminRouter=require("./routes/adminRouter")
 
 
 const app=express();
-app.use(express.static(path.resolve(__dirname,"dist")))
 dbConnection();
+app.use(express.static(path.resolve(__dirname,"dist")))
 
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:5173","http://localhost:5174", 'https://ms-server-six.vercel.app']
+    origin:["http://localhost:5173","http://localhost:5174", 'https://ms-server-six.vercel.app/']
 }));
 app.use(cookieParser());
 app.use(express.json());
