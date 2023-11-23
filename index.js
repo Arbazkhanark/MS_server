6044c5220631ffa0d85e1b9d4da9f542329cdf9c
Arbaaz Khan <arbaazkhanark23@gmail.com>
2023-11-23 17:14:35 +0530

    try 8

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.resolve(__dirname,"dist")))
 
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:5173","http://localhost:5174", 'https://ms-server-six.vercel.app/']
+    origin:'*'
 }));
 app.use(cookieParser());
 app.use(express.json());
